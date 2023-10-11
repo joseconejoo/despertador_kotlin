64fcdbafcb65d8d1e5b75bdcbb16853d7143664c
Olwer Altuve <olwerjose33@hotmail.com>
2023-10-10 22:25:57 -0400

    Now Works!!! ALpha worked!!!

--- app/src/main/java/com/despertador_kotlin/MainActivity.kt
+++ app/src/main/java/com/despertador_kotlin/MainActivity.kt
@@ -1,7 +1,16 @@
 package com.despertador_kotlin
 
+import android.app.KeyguardManager
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,12 +23,47 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.despertador_kotlin.ui.theme.Despertador_kotlinTheme
 import android.provider.Settings
-
+import android.view.WindowManager
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import android.os.PowerManager
+import android.os.PowerManager.WakeLock
 
 class MainActivity : ComponentActivity() {
     private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // pantalla_bloqueada_start
+        val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true)
+            setTurnScreenOn(true)
+            keyguardManager.requestDismissKeyguard(this, null)
+        } else {
+            window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON)
+        }
+        // pantalla_bloqueada_end
+        // bateria_optimizacion_ignore_start
+        val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!powerManager.isIgnoringBatteryOptimizations(packageName)) {
+                // La aplicación no tiene permiso para ignorar las optimizaciones de batería
+                // Solicitar al usuario que otorgue este permiso
+                val intent = Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS).apply {
+                    data = Uri.parse("package:$packageName")
+                }
+                startActivity(intent)
+            } else {
+                // La aplicación ya tiene permiso para ignorar las optimizaciones de batería
+                // No es necesario hacer nada
+            }
+        }
+        // bateria_optimizacion_ignore_end
+        // layout_start
+        setContentView(R.layout.activity_main)
+        // layout_end
+
         super.onCreate(savedInstanceState)
         val notificationListenerEnabled = Settings.Secure.getString(this.contentResolver, "enabled_notification_listeners")
         val packageName = packageName
@@ -50,12 +94,21 @@ class MainActivity : ComponentActivity() {
         setContent {
             Despertador_kotlinTheme {
                 // A surface container using the 'background' color from the theme
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Greeting("this is an alarm")
+                        MyApp()
+                    }
                 }
+
             }
         }
     }
@@ -69,12 +122,29 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+@Composable
+fun MyApp() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Button(onClick = {
+        /* Aquí va el código que se ejecutará al presionar el botón */
+            NotificationListener.notificationPresent = false
+        }) {
+            Text("Apagar")
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Despertador_kotlinTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            MyApp()
+        }
+        //Greeting("Android12")
     }
 }
 
--- app/src/main/java/com/despertador_kotlin/NotificationListener.kt
+++ app/src/main/java/com/despertador_kotlin/NotificationListener.kt
@@ -5,13 +5,24 @@ import android.media.MediaPlayer
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
 import android.media.AudioManager
+import android.os.Build
+import android.os.PowerManager
 import android.util.Log
-
+import android.view.WindowManager
+import androidx.core.app.NotificationCompat
+import android.provider.Settings
 class NotificationListener : NotificationListenerService() {
     private lateinit var mediaPlayer: MediaPlayer
-    private var notificationPresent = false
+    companion object {
+        var notificationPresent = false
+    }
     private lateinit var handler: Handler
     private lateinit var runnable: Runnable
 
@@ -20,8 +31,15 @@ class NotificationListener : NotificationListenerService() {
         val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
         audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume, 0)
     }
+    private fun checkActiveNotifications() {
+        val activeNotifications = getActiveNotifications()
+        for (sbn in activeNotifications) {
+            handleNotification(sbn)
+        }
+    }
     override fun onCreate() {
         super.onCreate()
+
         Log.d("NotificationListenerOnCreate", "Notification text: ")
 
         mediaPlayer = MediaPlayer.create(applicationContext, R.raw.alarm_sound)
@@ -33,9 +51,10 @@ class NotificationListener : NotificationListenerService() {
                     setVolumeToMax()
                     mediaPlayer.start()
                 }
+                checkActiveNotifications()
                 //handler.postDelayed(this, 60000)
                 // 60 segundos arriba
-                handler.postDelayed(this, 1000)
+                handler.postDelayed(this, 30000)
             }
         }
         handler.post(runnable)
@@ -45,25 +64,72 @@ class NotificationListener : NotificationListenerService() {
         super.onListenerConnected()
         Log.d("activeNotif_0", "Notification text: 0")
         // Maneja -las notificaciones existentes
-        val activeNotifications = getActiveNotifications()
-        Log.d("activeNotif_1.0", "Notification text: $activeNotifications")
-        for (sbn in activeNotifications) {
-            Log.d("activeNotif_1.1", "Notification text: $sbn")
-            handleNotification(sbn)
-        }
+        checkActiveNotifications()
     }
 
     private fun handleNotification(sbn: StatusBarNotification) {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (!notificationManager.isNotificationListenerAccessGranted(ComponentName(this, NotificationListener::class.java))) {
+            val intent = Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS)
+            startActivity(intent)
+        }
         val notification = sbn.notification
         val extras = notification.extras
-        val text = extras.getCharSequence(Notification.EXTRA_TEXT)?.toString()?.lowercase()
+        //val text = extras.getCharSequence(Notification.EXTRA_TEXT)?.toString()?.lowercase()
+        //val extras = notification.extras
+        val title = extras.getCharSequence(Notification.EXTRA_TITLE)?.toString()?.lowercase()
+        val text_normal = extras.getCharSequence(Notification.EXTRA_TEXT)?.toString()?.lowercase()
+        val subText = extras.getCharSequence(Notification.EXTRA_SUB_TEXT)?.toString()?.lowercase()
+        val text = listOfNotNull(title, text_normal, subText).joinToString(" ")
         Log.d("NotificationListener", "Notification text: $text")
-        if (text != null && text.contains("pattern,", true)) {
+        //if (text != null && text.contains("pattern,", true)) {
+        //if (text != null && text.contains("prog1", true)) {
+        if (text.contains("prog1", ignoreCase = true)) {
             notificationPresent = true
             if (!mediaPlayer.isPlaying) {
                 setVolumeToMax()
-
                 mediaPlayer.start()
+
+                val notificationIntent = Intent(this, MainActivity::class.java)
+                notificationIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+                val pendingIntent = PendingIntent.getActivity(
+                    this,
+                    0,
+                    notificationIntent,
+                    PendingIntent.FLAG_IMMUTABLE
+                )
+
+                val notification = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    val channel = NotificationChannel(
+                        "channel_id",
+                        "Channel Name",
+                        NotificationManager.IMPORTANCE_DEFAULT
+                    )
+                    val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                    notificationManager.createNotificationChannel(channel)
+
+                    NotificationCompat.Builder(this, "channel_id")
+                        .setContentTitle("Mi aplicación de despertador")
+                        .setContentText("Reproduciendo música")
+                        .setContentIntent(pendingIntent)
+                        .setOngoing(true)
+                        .build()
+                } else {
+                    NotificationCompat.Builder(this)
+                        .setContentTitle("Mi aplicación de despertador")
+                        .setContentText("Reproduciendo música")
+                        .setContentIntent(pendingIntent)
+                        .setOngoing(true)
+                        .build()
+                }
+
+                // encender pantalla
+                val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+                val wakeLock = powerManager.newWakeLock(
+                    PowerManager.PARTIAL_WAKE_LOCK or PowerManager.ACQUIRE_CAUSES_WAKEUP,
+                    "MyApp:WakeLock"
+                )
+                wakeLock.acquire(3000) // Duración en milisegundos para mantener la pantalla encendida
             }
         }
     }
@@ -82,7 +148,7 @@ class NotificationListener : NotificationListenerService() {
         val notification = sbn.notification
         val extras = notification.extras
         val text = extras.getCharSequence(Notification.EXTRA_TEXT)?.toString()?.lowercase()
-        if (text != null && text.contains("virtual", true)) {
+        if (text != null && text.contains("prog1", true)) {
             notificationPresent = false
             if (mediaPlayer.isPlaying) {
                 mediaPlayer.stop()
